4c23daf1e20ee0478afb139fa816a34c061d11e2
Luã Álvaro Lage Carlos <lua.alvaro@ubistart.com>
2022-06-09 18:40:44 -0300

    video finalizado

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,26 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+  if (req.method !== "POST")
+    return res.status(400).json({ message: 'Método inválido' })
+
+  const { number } = JSON.parse(req.body)
+  console.log(number)
+
+  const randomNumber = Math.floor(Math.random() * 10 + 1)
+
+  if (number === randomNumber) {
+    res.status(200).json({
+      message: "Parabéns! Você acertou.",
+      randomNumber,
+      status: true,
+    })
+  } else {
+    res.status(200).json({
+      message: "Poxaaa, que pena. Tente novamente!",
+      randomNumber,
+      status: false,
+    })
+  }
+
+
 }
